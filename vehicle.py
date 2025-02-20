63104fa6860c06314cf229468040cc2f25bee429
Mahendran4226 <mahendran4226@gmail.com>
2025-02-20 17:46:17 +0530

    qa qa
    	qa
    	#qa
    
    :qa

---
+++ vehicle.py
@@ -0,0 +1,11 @@
+class Vehicle:
+    def __init__(self, make, model, year, weight):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.weight = weight
+
+    def get_make(self):
+
+        return self.make
+    
